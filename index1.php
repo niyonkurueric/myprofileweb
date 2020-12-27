c5e68607be006b62d3572cd16789615efae17d8e
niyonkurueric <niyonkurufamous@gmail.com>
2020-12-27 20:47:20 +0200

    my name

---
+++ index1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>niyonkuru eric</title>
+	<link rel="stylesheet" type="text/css" href="css\my profile.css">
+</head>
+<body>
+	<div class="image">
+		<img src="">
+		
+	</div>
+	<div class="name">
+		<h1>ERIC</h1>
+	</div>
+
+</body>
+</html>
